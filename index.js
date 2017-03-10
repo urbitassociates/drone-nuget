5d4975d6b8551be5c9f357cc46f7de3cbfbef505
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2017-03-10 12:25:25 +0100

    Improve code cosmetics * Restructure code, put plugin entry point first * Add doc comments * Refactor method & variable names to be more JS idiomatic

--- index.js
+++ index.js
@@ -1,86 +1,98 @@
-const Drone = require('drone-node');
-const plugin = new Drone.Plugin();
-
-const Nuget = require('nuget-runner');
-
-const path = require('path');
+const path    = require('path');
 const shelljs = require('shelljs');
+const Drone   = require('drone-node');
+const Nuget   = require('nuget-runner');
 
-const do_push = function (nuget, workspace, vargs, file) {
-  var relative_path = path.relative('.', file);
+const plugin  = new Drone.Plugin();
+
+/**
+ * Plugin entrypoint
+ */
+plugin.parse().then((params) => {
+  // gets build, repository, workspace and plugin-specific params for the current running build
+  const { build, repo, workspace, vargs } = params;
+
+  vargs.verbosity || (vargs.verbosity = 'quiet');
+  vargs.files     || (vargs.files = []);
+
+  uploadPackage(workspace, vargs);
+});
+
+/**
+ * Upload the NuGet package
+ *
+ * @param workspace
+ * @param vargs
+ */
+const uploadPackage = function (workspace, vargs) {
+    if (vargs.source) {
+        const nugetPath = '/usr/lib/nuget/NuGet.exe';
+        const nuget = new Nuget({
+            apiKey: vargs.api_key,
+            verbosity: vargs.verbosity,
+            nugetPath: nugetPath
+        });
+
+        const nugetVersion = shelljs.exec('mono ' + nugetPath, {silent:true}).head({'-n': 1}).stdout;
+        console.log(nugetVersion);
+
+        const resolvedFiles = [].concat.apply([], vargs.files.map((f) => { return shelljs.ls(workspace.path + '/' + f); }));
+        resolvedFiles.forEach((file) => {
+            if (path.extname(file) === '.nuspec') {
+                packThenPushPackage(nuget, workspace, vargs, file);
+            } else {
+                pushPackage(nuget, workspace, vargs, file);
+            }
+        });
+    } else {
+        console.error("Parameter missing: NuGet source URL");
+        process.exit(1);
+    }
+};
+
+/**
+ * Packs then pushes the NuGet package to the NuGet or MyGet repo
+ *
+ * @param nuget
+ * @param workspace
+ * @param vargs
+ * @param file
+ */
+const packThenPushPackage = function (nuget, workspace, vargs, file) {
+    console.log('Start packing ' + file);
+    nuget.pack({
+        spec: file,
+        outputDirectory: path.dirname(file)
+    }).then(function(stdout) {
+        console.log('Successfully packed ' + file);
+
+        const packagePath = file.replace('.nuspec', '*.nupkg')
+        const resolvedPackageFile = shelljs.ls(packagePath)
+        pushPackage(nuget, workspace, vargs, resolvedPackageFile[0]);
+    }).catch(function (err) {
+        console.error('An error happened while packing: ' + err);
+        process.exit(1);
+    });
+};
+
+/**
+ * Pushes the NuGet package to the NuGet or MyGet repo
+ *
+ * @param nuget
+ * @param workspace
+ * @param vargs
+ * @param file
+ */
+const pushPackage = function (nuget, workspace, vargs, file) {
+  const relativePath = path.relative('.', file);
 
   console.log('Start pushing ' + file);
-  nuget.push(relative_path, {
+  nuget.push(relativePath, {
     source: vargs.source
   }).then(function (stdout) {
-
    console.log('Successfully pushed ' + file);
   }).catch(function (err) {
     console.error('An error happened while pushing: ' + err);
     process.exit(1);
   });
-}
-
-const do_pack_then_push = function (nuget, workspace, vargs, file) {
-  console.log('Start packing ' + file);
-  nuget.pack({
-    spec: file,
-    outputDirectory: path.dirname(file)
-  }).then(function(stdout) {
-    console.log('Successfully packed ' + file);
-    var package_path = file.replace('.nuspec', '*.nupkg')
-    var resolved_package_file = shelljs.ls(package_path)
-
-    do_push(nuget, workspace, vargs, resolved_package_file[0]);        
-
-  }).catch(function (err) {
-    console.error('An error happened while packing: ' + err);
-    process.exit(1);
-  });
-}
-
-const do_upload = function (workspace, vargs) {
-  if (vargs.source) {
-
-    var nugetPath = '/usr/lib/nuget/NuGet.exe';
-    var nuget = new Nuget({
-      apiKey: vargs.api_key,
-      verbosity: vargs.verbosity,
-      nugetPath: nugetPath
-    });
-
-    var nugetVersion = shelljs.exec('mono ' + nugetPath, {silent:true}).head({'-n': 1}).stdout;
-    console.log(nugetVersion);
-
-    var resolved_files = [].concat.apply([], vargs.files.map((f) => { return shelljs.ls(workspace.path + '/' + f); }));
-    resolved_files.forEach((file) => {
-
-      if (path.extname(file) === '.nuspec') {
-        do_pack_then_push(nuget, workspace, vargs, file);
-      } else {
-        do_push(nuget, workspace, vargs, file);        
-      }
-    });
-  } else {
-    console.error("Parameter missing: NuGet source URL");
-    process.exit(1);
-  }
-}
-
-plugin.parse().then((params) => {
-
-  // gets build and repository information for
-  // the current running build
-  const build = params.build;
-  const repo  = params.repo;
-  const workspace = params.workspace;
-
-  // gets plugin-specific parameters defined in
-  // the .drone.yml file
-  const vargs = params.vargs;
-
-  vargs.verbosity     || (vargs.verbosity = 'quiet');
-  vargs.files         || (vargs.files = []);
-
-  do_upload(workspace, vargs);
-});
+};
